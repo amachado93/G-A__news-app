647ce7a94e3ec4c7ce3b6af00ab0a65e04e3d909
Alex Machado <amachado@sva.edu>
2021-05-21 22:27:53 -0400

    Add styling to header and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 function Footer() {
   return (
     <div id="footer">
-      <div class="footer__left">
+      <div className="footer__left">
         <h4>Vandelay Industries</h4>
         <p>copywrite 202X</p>
       </div>
-      <div class="footer__right">
+      <div className="footer__right">
         <p>Check us out on...</p>
         <ul>
           <li>Facebook</li>
